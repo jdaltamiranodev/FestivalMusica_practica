cb8e85d49f0a8d732c644955d4b2e1e47cdf2a74
David Altamirano <jdaltamiranodev@gmail.com>
2024-02-08 00:50:44 -0500

    Se finaliza la configuración de SASS y Gulp y se deja todo listo para el inicio del proyecto.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const { src, dest, watch} = require("gulp")
 const sass = require("gulp-sass")(require('sass'));
+const plumber = require('gulp-plumber');
 
 function css( done ){
     // Identificar el archivo de SASS
@@ -9,15 +10,16 @@ function css( done ){
 
     // Almacenarla en el disco duro
 
-    src('src/scss/app.scss') // Identificar el archivo SASS
+    src('src/scss/**/*.scss') // Identificar el archivo SASS
+        .pipe( plumber())
         .pipe( sass() ) // Compilarlo
-        .pipe( dest('build/css') ); // Almacenarla en el disco duro
+        .pipe( dest('build/css') ) // Almacenarla en el disco duro
 
     done(); // callback que avisa a gulp cuando llegamos al final
 }
 
 function dev(done){
-    watch("src/scss/app.scss", css)
+    watch("src/scss/**/*.scss", css)
 
     done();
 }
